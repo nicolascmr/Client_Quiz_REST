c7f473b0a1ea42e973ea2fd3757c20e38b349dfa
Bastien-MOINS <bastien.moins@gmail.com>
2026-03-18 09:30:40 +0100

    Initialisation de la vue du joueur + question texte (60% complet)

---
+++ vue_quizz/src/components/QuestionsFormText.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="form-text">
+    <h3>{{ question_name }}</h3>
+    <input class="input" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  question_name: '',
+  props: {
+    input: '',
+    answer: ''
+  },
+
+  methods:{
+    checkTruthiness : function() {
+        this.$emit('isTrue',{answer: this.answer});
+    }
+  },
+  emits : ['isTrue']
+}
+</script>
---
+++ vue_quizz/src/views/PlayView.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="app-container">
+    <h1>Question n°1</h1>
+    
+    <QuestionsFormText
+      @isTrue="checkTruthiness"
+    />
+
+  <div class="valider">
+    <button @click="nextQuestion" v-if="isTrue" class="valide_button">✅ valider</button>
+  </div>
+  </main>
+</template>
+
+<script>
+import QuestionsFormQCM from '../components/QuestionsFormQCM.vue';
+import QuestionsFormText from '../components/QuestionsFormText.vue';
+
+export default {
+  name: 'PlayerView',
+  components: {
+    QuestionsFormQCM,
+    QuestionsFormText
+  },
+  data() {
+    return {
+      glasses: 0,
+      dailyGoal: 8,
+      questions: [],
+      score: 0,
+      scoreMax: 0
+    };
+  },
+  methods: {
+    setScoreMax(){
+
+    },
+    incrementScore(){
+
+    },
+    nextQuestion(){
+
+    },
+    checkTruthiness($event){
+        return this.input == $event.answer;
+    }
+  }
+}
+</script>
