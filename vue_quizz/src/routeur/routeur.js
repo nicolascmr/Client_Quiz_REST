ff56ea168cd998ec87743639620e5a27dfc62084
nicolascmr <nicolascmr14@gmail.com>
2026-03-31 23:05:08 +0200

    Ajout de la route sécurisée par mot de passe pour la page admin

--- vue_quizz/src/routeur/routeur.js
+++ vue_quizz/src/routeur/routeur.js
@@ -12,4 +12,30 @@ const router = createRouter({
     history: createWebHistory(),
     routes, 
 })
+
+let isAuthenticated = false;
+const password_saved = 'admin'; 
+
+router.beforeEach((to, from, next) => {
+    if (to.path.startsWith('/admin')) {
+        if (!isAuthenticated) {
+            const password = window.prompt("Veuillez entrer le mot de passe :");
+            
+            if (password_saved == password) {
+                isAuthenticated = true; 
+                next();
+            } else {
+                window.alert('Mot de passe incorrect ! Access refusé.');
+                if (!from.path === '/') {
+                    next(false);
+                }
+            }
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+})
+
 export default router
